e557142b27265680e9483870950effd5665e7b03
Konstantin Kožokar <konstantin.kozokar@gmail.com>
2021-11-23 09:02:04 +0100

    [ADD] Table responsivness

--- src/component/Tables/WeatherTable/WeatherTableComponent.tsx
+++ src/component/Tables/WeatherTable/WeatherTableComponent.tsx
@@ -10,42 +10,44 @@ interface ITableComponentProps{
     searchType: string
 }
 
-const TableComponent: React.FunctionComponent<ITableComponentProps> = function ({ data, searchType }) {
+const WeatherTableComponent: React.FunctionComponent<ITableComponentProps> = function ({ data, searchType }) {
   return (
-    <Table>
-      <thead>
-        <tr className="d-none d-lg-table-row">
-          <th scope="col">GCM scenario</th>
-          {MONTHS_SHORT.map((month) => <th key={month}>{month}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((record: IClimateDTO) => (
-          <tr className="d-block d-lg-table-row mt-3 mt-lg-0" key={record.gcm}>
-            <td className="d-block d-lg-table-cell">
-              <strong className="d-inline d-lg-none">
-                GCM scenario:
-                {' '}
-              </strong>
-              {getGCMDisplayName(record.gcm)}
-            </td>
-            {record.monthVals.map((val, index) => (
-              <td className="d-block d-lg-table-cell" key={`${record.gcm}-${val}`} title={`${val}`}>
+    <div className="table-responsive">
+      <Table>
+        <thead>
+          <tr className="d-none d-lg-table-row">
+            <th scope="col">GCM scenario</th>
+            {MONTHS_SHORT.map((month) => <th key={month}>{month}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((record: IClimateDTO) => (
+            <tr className="d-block d-lg-table-row mt-3 mt-lg-0" key={record.gcm}>
+              <td className="d-block d-lg-table-cell">
                 <strong className="d-inline d-lg-none">
-                  {MONTHS_SHORT[index]}
-                  :
+                  GCM scenario:
                   {' '}
                 </strong>
-                {round(val)}
-                {searchType === 'tas' && <>&#8451;</>}
-                {searchType === 'pr' && <>mm</>}
+                {getGCMDisplayName(record.gcm)}
               </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+              {record.monthVals.map((val, index) => (
+                <td className="d-block d-lg-table-cell" key={`${record.gcm}-${val}`} title={`${val}`}>
+                  <strong className="d-inline d-lg-none">
+                    {MONTHS_SHORT[index]}
+                    :
+                    {' '}
+                  </strong>
+                  {round(val)}
+                  {searchType === 'tas' && <>&#8451;</>}
+                  {searchType === 'pr' && <>mm</>}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
-export default TableComponent;
+export default WeatherTableComponent;
